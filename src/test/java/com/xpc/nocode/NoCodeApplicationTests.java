9952ffd0a7794b8f22ee6053ec28d1926bcb0a99
徐鹏程 <1489588006@qq.com>
2026-06-09 14:18:19 +0800

    初始化

---
+++ src/test/java/com/xpc/nocode/NoCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xpc.nocode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
