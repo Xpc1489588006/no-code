bd275d9835c8d7f9d6d9643892389c256ce2aa65
徐鹏程 <1489588006@qq.com>
2026-06-09 14:36:57 +0800

    通用基础代码

--- src/main/java/com/xpc/nocode/NoCodeApplication.java
+++ src/main/java/com/xpc/nocode/NoCodeApplication.java
@@ -2,8 +2,9 @@ package com.xpc.nocode;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class NoCodeApplication {
 
     public static void main(String[] args) {
