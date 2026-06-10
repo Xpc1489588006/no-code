b9a54b519c1baa13797d84e5158a9e4f278da903
徐鹏程 <1489588006@qq.com>
2026-06-10 19:27:59 +0800

    AI生成应用

---
+++ src/test/java/com/xpc/nocode/ai/AiCodeGeneratorServiceTest.java
@@ -0,0 +1,28 @@
+package com.xpc.nocode.ai;
+
+import com.xpc.nocode.ai.model.HtmlCodeResult;
+import com.xpc.nocode.ai.model.MultiFileCodeResult;
+import jakarta.annotation.Resource;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiCodeGeneratorServiceTest {
+
+    @Resource
+    private AiCodeGeneratorService aiCodeGeneratorService;
+
+    @Test
+    void generateHtmlCode() {
+        HtmlCodeResult result = aiCodeGeneratorService.generateHtmlCode("做个橡皮擦的工作记录小工具");
+        Assertions.assertNotNull(result);
+    }
+
+    @Test
+    void generateMultiFileCode() {
+        MultiFileCodeResult multiFileCode = aiCodeGeneratorService.generateMultiFileCode("做个橡皮擦的留言板");
+        Assertions.assertNotNull(multiFileCode);
+    }
+}
+
