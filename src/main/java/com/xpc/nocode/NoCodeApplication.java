92643254363330eb04372391e32322400d6381ec
徐鹏程 <1489588006@qq.com>
2026-06-10 16:26:12 +0800

    用户模块

--- src/main/java/com/xpc/nocode/NoCodeApplication.java
+++ src/main/java/com/xpc/nocode/NoCodeApplication.java
@@ -1,10 +1,11 @@
 package com.xpc.nocode;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication
+@MapperScan("com.xpc.nocode.mapper")
 public class NoCodeApplication {
 
     public static void main(String[] args) {
