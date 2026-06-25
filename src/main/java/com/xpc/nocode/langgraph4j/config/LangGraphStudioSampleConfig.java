b2a9f230c6bf8377cf04aac762c5c336ecd710bf
徐鹏程 <1489588006@qq.com>
2026-06-25 19:55:31 +0800

    集成LangGraph4j

---
+++ src/main/java/com/xpc/nocode/langgraph4j/config/LangGraphStudioSampleConfig.java
@@ -0,0 +1,25 @@
+package com.xpc.nocode.langgraph4j.config;
+
+
+/**
+ * 可视化调试配置（实际上很难使用）
+ */
+//@Configuration
+//public class LangGraphStudioSampleConfig extends AbstractLangGraphStudioConfig {
+//
+//    final LangGraphFlow flow;
+//
+//    public LangGraphStudioSampleConfig() throws GraphStateException {
+//        var workflow = new CodeGenWorkflow().createWorkflow().stateGraph;
+//        // define your workflow
+//        this.flow = LangGraphFlow.builder()
+//                .title("LangGraph Studio")
+//                .stateGraph(workflow)
+//                .build();
+//    }
+//
+//    @Override
+//    public LangGraphFlow getFlow() {
+//        return this.flow;
+//    }
+//}
