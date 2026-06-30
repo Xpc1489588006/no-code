ddba297d1e006d02093ede1628faaac364959d5d
徐鹏程 <1489588006@qq.com>
2026-06-30 17:24:44 +0800

    稳定性 ：输出护轨和工具调用次数限制

--- src/main/java/com/xpc/nocode/ai/AiCodeGeneratorServiceFactory.java
+++ src/main/java/com/xpc/nocode/ai/AiCodeGeneratorServiceFactory.java
@@ -114,7 +114,9 @@ public class AiCodeGeneratorServiceFactory {
                                 ToolExecutionResultMessage.from(toolExecutionRequest,
                                         "Error: there is no tool called " + toolExecutionRequest.name())
                         )
+                        .maxSequentialToolsInvocations(20)  // 最多连续调用 20 次工具
                         .inputGuardrails(new PromptSafetyInputGuardrail()) // 添加输入护轨
+             //         .outputGuardrails(new RetryOutputGuardrail()) // 添加输出护轨，为了流式输出，这里不使用
                         .build();
             }
             // HTML 和 多文件生成，使用流式对话模型
@@ -125,7 +127,9 @@ public class AiCodeGeneratorServiceFactory {
                         .chatModel(chatModel)
                         .streamingChatModel(openAiStreamingChatModel)
                         .chatMemory(chatMemory)
+
                         .inputGuardrails(new PromptSafetyInputGuardrail()) // 添加输入护轨
+        //              .outputGuardrails(new RetryOutputGuardrail()) // 添加输出护轨，为了流式输出，这里不使用
                         .build();
             }
             default ->
