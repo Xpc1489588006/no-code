594b69109f3bbb0d371991cedd34de85e5431177
徐鹏程 <1489588006@qq.com>
2026-06-30 16:26:10 +0800

    vue项目实时浏览

--- src/main/java/com/xpc/nocode/core/handler/JsonMessageStreamHandler.java
+++ src/main/java/com/xpc/nocode/core/handler/JsonMessageStreamHandler.java
@@ -6,8 +6,6 @@ import cn.hutool.json.JSONUtil;
 import com.xpc.nocode.ai.model.message.*;
 import com.xpc.nocode.ai.tools.BaseTool;
 import com.xpc.nocode.ai.tools.ToolManager;
-import com.xpc.nocode.constant.AppConstant;
-import com.xpc.nocode.core.builder.VueProjectBuilder;
 import com.xpc.nocode.model.entity.User;
 import com.xpc.nocode.model.enums.ChatHistoryMessageTypeEnum;
 import com.xpc.nocode.service.ChatHistoryService;
@@ -27,8 +25,6 @@ import java.util.Set;
 @Component
 public class JsonMessageStreamHandler {
 
-    @Resource
-    private VueProjectBuilder vueProjectBuilder;
 
     @Resource
     private ToolManager toolManager;
@@ -60,9 +56,7 @@ public class JsonMessageStreamHandler {
                     // 流式响应完成后，添加 AI 消息到对话历史
                     String aiResponse = chatHistoryStringBuilder.toString();
                     chatHistoryService.addChatMessage(appId, aiResponse, ChatHistoryMessageTypeEnum.AI.getValue(), loginUser.getId());
-                    // 异步构造 Vue 项目
-                    String projectPath = AppConstant.CODE_OUTPUT_ROOT_DIR + "/vue_project_" + appId;
-                    vueProjectBuilder.buildProjectAsync(projectPath);
+
                 })
                 .doOnError(error -> {
                     // 如果AI回复失败，也要记录错误消息
